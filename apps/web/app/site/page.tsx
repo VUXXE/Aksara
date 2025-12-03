fee7897b96c6a1320e1ad1d9af6c3856e01ffdea
Exuvia <Hanan7taqiyya@gmail.com>
2025-12-04 03:23:16 +0700

    fix: add ids to landing page sections for navbar navigation

--- apps/web/app/site/page.tsx
+++ apps/web/app/site/page.tsx
@@ -19,7 +19,7 @@ const HomePage: React.FC = () => {
         <HeroSection />
         <BackgroundBeams className="opacity-40" />
       </section>
-      <section>
+      <section id="pricing">
         <MaxWidthWrapper className="flex items-center flex-col gap-4 md:mt-20">
           <h2 className="text-4xl text-center font-medium">
             Choose what fits you right
@@ -31,10 +31,10 @@ const HomePage: React.FC = () => {
           <HoverPriceCard />
         </MaxWidthWrapper>
       </section>
-      <section className="w-full mt-10 md:mt-20">
+      <section id="solutions" className="w-full mt-10 md:mt-20">
         <InfiniteMovingCards pauseOnHover={false} speed="slow" />
       </section>
-      <section className="w-full mt-10 md:mt-20">
+      <section id="features" className="w-full mt-10 md:mt-20">
         <MaxWidthWrapper>
           <div className="flex flex-col gap-4 items-center">
             <h2 className="text-4xl text-center font-medium">
@@ -53,7 +53,7 @@ const HomePage: React.FC = () => {
           <StickyScroll />
         </div>
       </section>
-      <div className="h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased">
+      <div id="about" className="h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4">
           <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
             Join Aksara
